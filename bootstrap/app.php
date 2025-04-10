31de538eb494c8d95564d7462c72675fc2253f4c
nikkajapos <japosnikka03@gmail.com>
2025-04-11 01:23:09 +0800

    jwt

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -89,7 +89,7 @@ $app->configure('app');
 | totally optional, so you are not required to uncomment this line.
 |
 */
-
+$app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 //$app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
